cd82edd13c225a8e0451e68d786a8cbf7ac2f822
Lucio Tato <luciotato@gmail.com>
2025-08-26 13:31:10 -0300

    test view.sh

--- scripts/build-mpdao-vote.sh
+++ scripts/build-mpdao-vote.sh
@@ -1,5 +1,5 @@
 echo "use cargo near build to build the contracts"
 cd meta-vote-contract
-cargo near build non-reproducible-wasm && cp target/near/meta_vote_contract/meta_vote_contract.wasm res/
+cargo near build non-reproducible-wasm && cp ../target/near/meta_vote_contract/meta_vote_contract.wasm ../res/
 cd ..
 
